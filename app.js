e2aeedfe384cb2b9e3223ceab8f58237a8bce2ff
SSKSHYAMSUNDAR <sskshyamsundar00@gmail.com>
2026-05-18 12:30:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const TABLE_NAME = process.env.TABLE_NAME || "StudentTable";
 
 // Home route
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'form.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Save student
